3e48c9bca3456632a173ccdd06fb3efffd776094
edwxrd7 <156730066+edwxrd7@users.noreply.github.com>
2024-05-12 21:52:55 -0400

    Add files via upload

--- jobdesc.js
+++ jobdesc.js
@@ -1,7 +1,4 @@
-const $cfaJob = $('#cfa-job');
-const $cfa = $('#cfa');
-
-
-$cfaJob.on('click', () => {
-    $cfa.removeClass('hide');
-  });
+$('#cfa-job').on('click', () => {
+    console.log('Clicked #cfa-job'); // Check if this message appears in the console
+    $('#cfa').removeClass('hide');
+})
